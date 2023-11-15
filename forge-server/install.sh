59fa688f226db212158c64eff20e73db9f617dde
FlamingH0rse <99073459+FlamingH0rse@users.noreply.github.com>
2023-11-15 15:14:36 +0530

    setup java added

--- forge-server/install.sh
+++ forge-server/install.sh
@@ -1,2 +1,2 @@
 java -jar forge-1.12.2-14.23.5.2859-installer.jar --installServer
-wget -P /var/cache/foobar/ -i mods.txt
+wget -P ./mods -i mods.txt
--- setup-ngrok.sh
+++ setup-ngrok.sh
@@ -1,2 +1,16 @@
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+RESET='\033[0m'
+
 curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
-echo "Visit https://dashboard.ngrok.com/get-started/setup/linux to get auth-token"
+
+export PATH="/usr/local/bin:$PATH"
+
+echo "${GREEN}Visit ${YELLOW}https://dashboard.ngrok.com/get-started/setup/linux ${GREEN}to get auth-token${RESET}"
+echo "${GREEN}Enter auth command below:${RESET}"
+read command
+$command
+
+screen -dmS "ngrok-mc" "ngrok tcp 25565"
+echo "${GREEN}Use ${YELLOW}\"screen -r ngrok\" ${GREEN}to view your ngrok IP${RESET}"
