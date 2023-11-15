c7bc53026ccf7d1415d1e4f317e3835403c2107a
FlamingH0rse <99073459+FlamingH0rse@users.noreply.github.com>
2023-11-15 14:55:22 +0530

    A

---
+++ setup-ngrok.sh
@@ -0,0 +1,2 @@
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
+echo "Visit https://dashboard.ngrok.com/get-started/setup/linux to get auth-token"
